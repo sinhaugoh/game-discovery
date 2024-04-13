bba3c6738e999e9d09de9586c9ef02da776d23bf
sinhaugoh <sinhaugoh456@hotmail.com>
2024-04-13 14:57:39 +0800

    style: implement game card skeleton for loading

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <Box overflow="hidden" borderRadius={10} width="250px">
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,21 +24,28 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((data) => setGames(data.data.results))
+      .then((data) => {
+        setGames(data.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
