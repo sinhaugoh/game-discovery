667ec16549dda2977dd35ef06891eb76be2a0b06
sinhaugoh <sinhaugoh456@hotmail.com>
2024-04-13 18:24:07 +0800

    feat: filter games by platform

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,35 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import usePlatforms from "../hooks/usePlatforms";
+
+interface Props {
+  selectedPlatform: Platform | null;
+  onSelectPlatform: (platform: Platform) => void;
+}
+
+const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedPlatform?.name ?? "Platform"}
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuItem
+            key={platform.id}
+            onClick={() => onSelectPlatform(platform)}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlatformSelector;
