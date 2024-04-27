42a4b1fe1538fd51e9d52ae13affa9202bafa334
sinhaugoh <sinhaugoh456@hotmail.com>
2024-04-27 11:47:59 +0800

    style: improve styling of game card and grid

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -47,12 +47,15 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
               boxSize="32px"
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
+              objectFit="cover"
             />
             <Button
               fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
               variant="link"
+              whiteSpace="normal"
+              textAlign="left"
             >
               {genre.name}
             </Button>
