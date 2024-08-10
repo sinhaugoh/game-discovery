67b4783de791f85c78141c48be6b543697e86450
sinhaugoh <sinhaugoh456@hotmail.com>
2024-08-10 17:37:42 +0800

    style: add error page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../pages/ErrorPage";
 import GameDetailpage from "../pages/GameDetailpage";
 import HomePage from "../pages/HomePage";
 import Layout from "../pages/Layout";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "game/:id", element: <GameDetailpage /> },
