285b2aef9393e1af27aa40f1441a424ca7991905
sinhaugoh <sinhaugoh456@hotmail.com>
2024-09-08 23:26:48 +0800

    refactor: move entity interfaces

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -9,7 +9,8 @@ import {
   SkeletonText,
   Text,
 } from "@chakra-ui/react";
-import useGenres, { Genre } from "../hooks/useGenres";
+import useGenres from "../hooks/useGenres";
+import { Genre } from "../entities/Genre";
 import getCroppedImageUrl from "../services/image-url";
 import useGameQueryStore from "../store";
 
